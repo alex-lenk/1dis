a4024071b5cf5ca2834e485f41103952b883713d
AlexLenk <artlenk.ru@gmail.com>
2020-04-19 15:42:44 +0300

    Created table version

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,28 +1,10 @@
-//= vendor/jquery-3.5.0.slim.min.js
-//= vendor/jquery.maskedinput.min.js
+// = vendor/jquery-3.5.0.slim.min.js
+// = vendor/jquery.maskedinput.min.js
 //= vendor/glider.min.js
-/*
-* 0.
-* 1.
-* 2.
-* 3.
-* 4.
-* 5.
-* 6.
-* 7.
-* 8.
-* 9.
-* 10.
-*/
 
-
-$(document).ready(function () {
-    $('#call-form__tel').mask("+7 (999) 999-99-99");
-
-    $('.menu-toggle').click(function () {
-        $('body').toggleClass('menu-opened');
-    });
-});
+function clickHeaderToggle() {
+    document.querySelector('body').classList.toggle('menu-opened');
+}
 
 new Glider(document.querySelector('.main-info-slider__init'), {
     slidesToShow: 1,
@@ -33,4 +15,43 @@ new Glider(document.querySelector('.main-info-slider__init'), {
         prev: '.main-info-slider__prev',
         next: '.main-info-slider__next'
     }
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('.header-toggle').addEventListener("click", clickHeaderToggle);
+
+
+    [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+        var keyCode;
+
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 (___) ___ ____",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function (a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i !== -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function (a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type === "blur" && this.value.length < 5) this.value = ""
+        }
+
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+    });
 });
